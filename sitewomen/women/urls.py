6a2590c6cf3ed8bd2b33402da47d662769d0f35a
Olga Rumyanceva <nj89.89@mail,ru>
2026-04-18 21:20:49 +0300

    Lesson 7

--- sitewomen/women/urls.py
+++ sitewomen/women/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('cats/<int:cat_id>/', views.categories), # http://127.0.0.1:8000/cats/2/
     path('cats/<slug:cat_slug>/', views.categories_by_slug), # http://127.0.0.1:8000/cats/adfkd/
     # re_path(r"^archive/(?P<year>[0-9]{4})/", views.archive), # archive/4 числа/
-    path("archive/<year4: year>/", views.archive),
+    path("archive/<year4:year>/", views.archive),
 ]
